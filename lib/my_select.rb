dcb96c3d0b7ac9aad0bef0106b06e16d1f950f85
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-19 20:34:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,8 +3,9 @@ def my_select(collection)
  var = ""
  my_array = []
  while i < collection.size
-  var = yield collection[i]
-  my_array << var
+  if yield collection[i]
+    my_array << collection[i]
+  end
   i += 1
  end
  my_array
